0052b1d80ff4150b9e8da246f93bb153e53899bc
mojo <moritz.noebauer@gmail.com>
2013-04-06 11:48:55 +0200

    Once again removed c++11 features & switched to a Google Codestyle.

--- graphics_raw_mesh.hpp
+++ graphics_raw_mesh.hpp
@@ -1,76 +1,60 @@
 #ifndef MFL_GRAPHICS_RAW_MESH_INCLUDED
 #define MFL_GRAPHICS_RAW_MESH_INCLUDED
 
-#include "MFL_Graphics_Common.hpp"
+#include "graphics_common.hpp"
 
 #include <vector>
 #include <algorithm>
-#include <xfunctional>
 
-#include <glm\glm.hpp>
-#include <GL\glew.h>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
 
 GRAPHICS_BEGIN
 
-struct mesh;
+struct Mesh;
 
-struct raw_mesh{ // mesh without color or texture data
-public:
-	typedef GLushort						idx_t;
-	typedef glm::vec3						value_t;
+// mesh without color or texture data
+//TODO(mojo): Change to fit standards.
+struct RawMesh{
+	typedef GLushort	Index;
+	typedef glm::vec3	Vec3;
 
-	typedef std::vector<value_t>			value_vec_t;
-	typedef std::vector<idx_t>				idx_vec_t;
+	typedef std::vector<Index>	vector_Index;
+	typedef std::vector<Vec3>	vector_Vec3;
 
-//private:
-	value_vec_t	_position;
-	value_vec_t _normal;
-	idx_vec_t	_index;
+	RawMesh() : position_(), normal_(), index_() {}
 
-	friend mesh;
-
-public:
-	raw_mesh()
-		:_position(),_normal(),_index(){}
-	raw_mesh(raw_mesh const&  raw)
-		:_position(raw._position),
-		_normal(raw._normal),
-		_index(raw._index){}
-	explicit raw_mesh(raw_mesh&& raw)
-		:_position	(std::forward<raw_mesh::value_vec_t>(raw._position)	),
-		_normal		(std::forward<raw_mesh::value_vec_t>(raw._normal)	),
-		_index		(std::forward<raw_mesh::idx_vec_t>	(raw._index)	){}
-
-	void append(value_t&& p,value_t&& n){
-		_position.push_back(std::forward<value_t>(p));
-		_normal.push_back(std::forward<value_t>(n));
-	}
-	void append(const value_t& p,const value_t& n){
-		_position.push_back(p);
-		_normal.push_back(n);
+	void Append(Vec3 const& position,Vec3 const& normal) {
+		position_.push_back(position);
+		normal_.push_back(normal);
 	}
 
-	template <typename iter_t>
-	void append(const iter_t pfirst,const iter_t plast,
-		const iter_t nfirst,const iter_t nlast){
-		_position.insert(_position.end(),pfirst,plast);
-		_normal.insert(_normal.end(),nfirst,nlast);
+	template <typename Iterator>
+	void Append(Iterator const position_first,Iterator const position_last,
+                Iterator const normal_first,Iterator const normal_last) {
+		position_.insert(position_.end(),position_first,position_last);
+		normal_.insert(normal_.end(),normal_first,normal_last);
 	}
 
-	size_t size() const{
-		return _position.size();
+	size_t Size() const {
+        assert(position_.size() == normal_.size());
+		return position_.size();
 	}
-	size_t index_size() const{
-		return _index.size();
-	}
-
-	template <typename iter_t>
-	void index_append(const iter_t first,const iter_t last){
-
-		assert(_normal.size() == _position.size());
-		_index.insert(_index.end(),first,last);
+	size_t IndexSize() const {
+		return index_.size();
 	}
 
+	template <typename Iterator>
+	void IndexAppend(Iterator const first,Iterator const last) {
+        assert(position_.size() == normal_.size());
+		index_.insert(index_.end(),first,last);
+	}
+	//===========================
+    // members
+    //===========================
+	vector_Vec3 position_;
+	vector_Vec3 normal_;
+	vector_Index index_;
 };
 
 GRAPHICS_END
