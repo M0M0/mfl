c96c5e455b0b5b489c8e26e90e246b5f3ce4a2c9
mojo <moritz.noebauer@gmail.com>
2013-05-09 13:27:25 +0200

    unix newline and version define

--- common.hpp
+++ common.hpp
@@ -1,5 +1,9 @@
+#ifndef MFL_VERSION
+#define MFL_VERSION     1
+#endif
+
 #ifndef MFL_COMMON_INCLUDED
-#define MFL_COMMON_INCLUDED
+#define MFL_COMMON_INCLUDED MFL_VERSION
 
 #define MFL_BEGIN	namespace mfl {
 #define MFL_END		}
