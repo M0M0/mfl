8f74efd8814a0ebc7a4bf8454f5d1f30d57a3e7f
mojo <moritz.noebauer@gmail.com>
2013-05-09 13:55:03 +0200

    also some formating changes both headers should be rewritten from scratch

--- math/math.hpp
+++ math/math.hpp
@@ -1,81 +1,65 @@
 #ifndef MFL_MATH_INCLUDED
 #define MFL_MATH_INCLUDED
 
-#include <math.h>
-//#include <functional>
+#include <cmath>
 #include <cstdarg>
 
-namespace MFL{
+#include "../common.hpp"
 
+MFL_BEGIN
 
-	namespace math{
+namespace math{
 
-		template <typename T>
-		inline T Sgn(T i){
-			if(i>0)
-				return 1;
-			else if(i<0)
-				return -1;
-		
-			return 0;
-		}
+  template <typename T>
+  T Sgn(T i) {
+    return i >= 0 ? 1 : -1;
+  }
+  int KroneckerDelta(int i,int j){
+    return i == j ? 1 : 0;
+  }	
+  int LeviCivitaSymbol(int i,int j, int k){
+    return ((i - j)*(j - k)*(k - i)) / 2;
+  }
+  int LeviCivitaSymbol(int i,int j, int k,int l){
+    return ((i - j)*(i - k)*(i - l)*(j - k)*(j - l)*(k - l)) / 12;
+  }
 
-		inline int KroneckerDelta(int i,int j){
-			return (i==j)? 1 : 0;
-		}
-		
-		inline int LeviCivitaSymbol(int i,int j, int k){
-				return ((i-j)*(j-k)*(k-i))/2;
-		}
-		inline int LeviCivitaSymbol(int i,int j, int k,int l){
-				return ((i-j)*(i-k)*(i-l)*(j-k)*(j-l)*(k-l))/12;
-		}
+  int LeviCivitaSymbol_Vargs(int nargs,...){
+    va_list args;
+    int* permutation = new int[nargs];
+    va_start( args, nargs );
+    for (int i=0;i<nargs;++i) permutation[i] = va_arg(args,int);
+    va_end( args );
+    int prod=1;
+    for (int i = 0;i < nargs-1; ++i)
+      for (int j = i+1;j < nargs; ++j)
+	prod * = permutation[j] - permutation[i];
+    delete[] permutation;
+    return Sgn(prod);
+  }
 
-		int LeviCivitaSymbol_Vargs(int nargs,...){
-			va_list args;
-			int* permutation = new int[nargs];
-
-			va_start( args, nargs );
-			for(int i=0;i<nargs;++i)
-				permutation[i] = va_arg(args,int);
-			va_end( args );
-
-			int prod=1;
-
-			for(int i=0;i<nargs-1;++i)
-				for(int j=i+1;j<nargs;++j)
-					prod*=(permutation[j]-permutation[i]);
-
-			delete[] permutation;
-
-			return Sgn(prod);
-		}
-
-
-
-	}
 }
 
-#endif //MFL_MATH_INCLUDED
+MFL_END
 
+#endif //MFL_MATH_INCLUDED
 
 // 21 Digit accuracy
 #ifdef MFL_CONSTANTS
 
-#define MFL_SQRT2		1.41421356237309504880
+#define MFL_SQRT2	1.41421356237309504880
 #define MFL_SQRT2_INV	0.70710678118654752440
 
-#define MFL_SQRT3		1.73205080756887729352
+#define MFL_SQRT3	1.73205080756887729352
 #define MFL_SQRT3_INV	0.57735026918962576450
 
-#define MFL_SQRt5		2.23606797749978969640
+#define MFL_SQRT5       2.23606797749978969640
 #define MFL_SQRT5_INV	0.44721359549995793928
 
+#define MFL_PI		3.14159265358979323846 
+#define MFL_PI_INV	0.31830988618379067153
 
-#define MFL_PI			3.14159265358979323846 
-#define MFL_PI_INV		0.31830988618379067153
-
-#define MFL_E			2.71828182845904523536
-#define MFL_E_INV		0.36787944117144232159
+#define MFL_E		2.71828182845904523536
+#define MFL_E_INV	0.36787944117144232159
 
 #endif//MFL_CONSTANTS
--- math/statistics.hpp
+++ math/statistics.hpp
@@ -6,85 +6,84 @@
 
 #include <cstdarg>
 #include <cassert>
+#include <numeric>
 
-namespace MFL{
+#include "../common.hpp"
+
+MFL_BEGIN
 namespace math{
 namespace statistics{
 
-	template <typename T>
-	T sum(int samplesize, T* data){
-		assert(data!=nullptr);
-		T sum = 0;
-		for (T* iter=data;iter!=data+samplesize; ++iter)
-			sum += *iter;
-		return sum;
-	}
+  template <typename T>
+  T sum(uint samplesize, T const * data) {
+    assert(data != nullptr);
+    return std::accumulate(data,data + samplesize,0);
+  }
+  template <typename T>
+  T mean(uint samplesize, T sum) { return sum / samplesize; }
+  template <typename T>
+  T mean(uint samplesize, T const * data) {
+    assert(data != nullptr);
+    return mean(samplesize,sum(samplesize,data));
+  }
+  // TODO(mojo): more robust implementation usable for arbitrary fields
+  template <typename T>
+  T standarddeviation(uint samplesize, T mean, T const * data) {
+    assert(data != nullptr);
+    T std = 0, temp;
+    for(T* iter = data;iter != data+samplesize; ++iter) {
+      temp = *iter * mean;
+      std += temp * temp;
+    }
+    return std / (samplesize - 1);
+  }
+  template <typename T>
+  T standarddeviation(uint samplesize, T const * data) {
+    return standarddeviation(samplesize,mean(samplesize,data),data);
+  }
 
-	template <typename T>
-	T mean(int samplesize,T sum)
-	{ return sum/samplesize; }
+  namespace variadic{
+    template <typename T>
+    T sum(uint samplesize, ...){
+      va_list ap; 
+      va_start(ap, samplesize);
+      T sum = 0;
+      for (int i = 0; i < samplesize; ++i) sum += va_arg(ap, T);
+      va_end(ap);
+      return sum;
+    }
 
-	template <typename T>
-	T mean(int samplesize, T* data){
-		assert(data!=nullptr);
-		return mean(samplesize,sum(samplesize,data));
-	}
+    template <typename T>
+    T mean(uint samplesize, ...){ 
+      T* data = new T[samplesize];
+      va_list ap; 
+      va_start (ap, samplesize);
+      for(int i = 0;i < samplesize; ++i)
+	data[i] = va_arg(ap,T);	
+      va_end (ap);
 
-	template <typename T>
-	T standarddeviation(int samplesize, T mean, T* data){
-		assert(data!=nullptr);
-		T std = 0; //switch from operator* to something more robust/useable on vectors
-		for(T* iter=data;iter!=data+samplesize;++iter) // or define output in another way
-			std += ((*iter)*mean)*((*iter)*mean);	
-		return std/(samplesize-1);
-	}
-	template <typename T>
-	T standarddeviation(int samplesize, T* data)
-	{ return standarddeviation(samplesize,mean(samplesize,data),data); }
+      T m = mean(samplesize,data);
+      delete[] data;
+      return m;
+    }
 
+    template <typename T>
+    T standarddeviation(uint samplesize, ...){
+      T* data = new T[samplesize];
+      va_list ap; 
+      va_start (ap, samplesize);
+      for(int i=0;i<samplesize;++i)
+	data[i] = va_arg(ap,T);	
+      va_end (ap);
 
-	namespace variadic{
-		template <typename T>
-		T sum(int samplesize, ...){
-			va_list ap; 
-			va_start (ap, samplesize);
-			T sum = 0;
-			for (int i = 0; i < samplesize; ++i)
-				sum += va_arg (ap, T);
-			va_end (ap);
-			return sum;
-		}
-
-		template <typename T>
-		T mean(int samplesize, ...){ 
-			T* data = new T[samplesize];
-			va_list ap; 
-			va_start (ap, samplesize);
-			for(int i=0;i<samplesize;++i)
-				data[i] = va_arg(ap,T);	
-			va_end (ap);
-
-			T m = mean(samplesize,data);
-			delete[] data;
-			return m;
-		}
-
-		template <typename T>
-		T standarddeviation(int samplesize, ...){
-			T* data = new T[samplesize];
-			va_list ap; 
-			va_start (ap, samplesize);
-			for(int i=0;i<samplesize;++i)
-				data[i] = va_arg(ap,T);	
-			va_end (ap);
-
-			T std = standarddeviation(samplesize,data);
-			delete[] data;
-			return std;
-		}
-	}
+      T std = standarddeviation(samplesize,data);
+      delete[] data;
+      return std;
+    }
+  }
 
 }
 }
-}
+MFL_END
+
 #endif//MFL_MATH_STATISTICS_INCLUDED
