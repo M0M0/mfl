7e94214fc7e24261f041fa359806cfbc8b3c4216
mojo <moritz.noebauer@gmail.com>
2013-05-11 21:49:05 +0200

    minor changes to Delta*seconds functions added a private method for simplification

--- utility/timer.hpp
+++ utility/timer.hpp
@@ -12,8 +12,6 @@ MFL_BEGIN
 /// the ridiculous syntax of chrono. Has pausing functionality.
 /// \author mojo
 class Timer{
-  typedef std::chrono::system_clock	   Clock_t;
-  typedef std::chrono::time_point<Clock_t> TimePoint;
  public:
   Timer() : time_saved_(),pause_(false) {
     Start();
@@ -21,7 +19,7 @@ class Timer{
   /// Saves the current time.
   /// \details
   void Start() {
-    time_saved_ = Clock_t::now();
+    time_saved_ = std::chrono::system_clock::now();
   }
   /// Returns the current state.
   /// \details An instance may either be unpaused(default) or paused.
@@ -31,7 +29,6 @@ class Timer{
   }
    /// Switches the instance into an paused state.
   /// \details If a instance is paused any call to a Delta function return 0.
-  /// \sa DeltaMS and DeltaS
   void Pause() {
     pause_ = true;
   }
@@ -41,59 +38,53 @@ class Timer{
     pause_ = false;
     Start();
   }
-  
-  /// Returns the timedifference in milliseconds since
-  /// the last call to Start or Unpause.
-  /// \details This function returns 0 if the instance is paused.
-  /// The function will reset the timer!
-  /// \return Returns the timedifference. (The default return type is float.)
-  template <typename Time = float>
-  Time DeltaMS() {
-    using std::chrono::duration_cast;
-    using std::chrono::milliseconds;
-    if (pause_) return static_cast<Time>(0);
-    auto time_end = Clock_t::now();
-    Time delta = static_cast<Time>(
-	         duration_cast<milliseconds>(time_end - time_saved_).count());
-    time_saved_ = time_end;
-    return delta;
-  }
   /// Returns the timedifference in seconds since
   /// the last call to Start or Unpause.
   /// \details This function returns 0 if the instance is paused.
   /// The function will reset the timer!
-  /// \return Returns the timedifference. (The default return type is float.)
-  template <typename Time = float>
-  Time DeltaS() {
-    using std::chrono::duration_cast;
+  /// \return Returns the timedifference. (The default return type is long.)
+  template <typename Time_t = long>
+  Time_t DeltaSeconds() {
     using std::chrono::seconds;
-    if (pause_) return static_cast<Time>(0);
-    auto time_end = Clock_t::now();
-    Time delta = static_cast<Time>(
-	         duration_cast<seconds>(time_end - time_saved_).count());
-    time_saved_ = time_end;
-    return delta;
+    return static_cast<Time_t>(_InternalDelta<seconds>());
+  }
+  /// Returns the timedifference in milliseconds since
+  /// the last call to Start or Unpause.
+  /// \details This function returns 0 if the instance is paused.
+  /// The function will reset the timer!
+  /// \return Returns the timedifference. (The default return type is long.)
+  template <typename Time_t = long>
+  Time_t DeltaMilliseconds() {
+    using std::chrono::milliseconds;
+    return static_cast<Time_t>(_InternalDelta<milliseconds>());
   }
   /// Returns the timedifference in microseconds since
   /// the last call to Start or Unpause.
   /// \details This function returns 0 if the instance is paused.
   /// The function will reset the timer!
-  /// \return Returns the timedifference. (The default return type is float.)
-  template <typename Time = float>
-  Time DeltaMicroS() {
-    using std::chrono::duration_cast;
+  /// \return Returns the timedifference. (The default return type is long.)
+  template <typename Time_t = long>
+  Time_t DeltaMicroseconds() {
     using std::chrono::microseconds;
-    if (pause_) return static_cast<Time>(0);
-    auto time_end = Clock_t::now();
-    Time delta = static_cast<Time>(
-	         duration_cast<microseconds>(time_end - time_saved_).count());
+    return static_cast<Time_t>(_InternalDelta<microseconds>());
+  }
+
+ private:
+  // Depends on the default type of duration cast!
+  // Right now it is long
+  template <typename Duration_t>
+  long _InternalDelta() {
+    using namespace std::chrono;
+    if (pause_) return 0;
+    auto time_end = system_clock::now();
+    auto delta = duration_cast<Duration_t>(time_end - time_saved_).count();
     time_saved_ = time_end;
     return delta;
-  } 
- 
- private:
-  TimePoint time_saved_; ///< TimePoint variable for storage of timedifference.
-  bool      pause_; ///< A boolean switch for simple pause functionality.
+  }
+
+  ///< TimePoint variable for storage of timedifference.
+  std::chrono::time_point<std::chrono::system_clock> time_saved_;
+  bool pause_; ///< A boolean switch for simple pause functionality.
 };
 
 MFL_END
